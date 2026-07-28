b8f06ac670c37c5f8fb7f6b89b619fd1d66a5a93
Liseno4eck <denisosen58@gmail.com>
2026-07-29 00:29:07 +0300

    Update and rename bot.py to test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import vk_api
 from vk_api.utils import get_random_id
 
 VK_TOKEN = 'vk1.a.j7YzvNPaYcJFfVUlTXUDnGeQDH3RVtJCRDIu16DenHlNBqJxBW1p6qJErZMDXRzfy41pVabgENQO8MBlcydzvsYqGepOhfHwnWBzeQ-PShrt2_HrhLQsOEXzUFjhxDAnqG_BNTSaFojr3He5Ctt_sFpmdQYSg-DdI6x--gmY5UpGwWFxXOvbYbs0Mg_ZrhdQNE7kJXAgLrbSWxXkMP7Kmg'
-CHAT_ID = 2000000419  # замените на ID вашей беседы
+CHAT_ID = 2000000419
 
 vk_session = vk_api.VkApi(token=VK_TOKEN)
 vk = vk_session.get_api()
