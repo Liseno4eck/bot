81134288a56bc437a0dc23a95e678007657f1070
Liseno4eck <denisosen58@gmail.com>
2026-07-29 07:47:12 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import threading
 import json
 from datetime import datetime
 
-*Конфигурация*
+#Конфигурация
 TOKEN = "vk1.a.baBit_arE7XyBbqXVkUrUYKMqCOw2zAZJ5_ZiFTyyW2hblfgfj0xadnRuTLIJSpa7G58feIA1p-UIt5ysnef1gwh4u78K3vV51Wc5IBmPLOJ5JyTO49wzxoWL1tMwtg5AQgC4QhV7Ka4tAJXgbqgVp75gQ39T11_72y4ZYiuTCgv36Sw8nrvcxOKPxcrW9Bme2Cx0UJDKud6S4bysNUO-w"
 GROUP_ID = 240350664  # ID сообщества, а не чата!
 OWNER_ID = 875762552
